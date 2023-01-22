ac25df8bc69cdf5ca5ba5384c446713df0a1614e
hussam <hasooooomi52@gmail.com>
2023-01-22 04:28:14 +0300

    nd

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Myapp());
+  runApp(const Myapp());
 }
 
 class Myapp extends StatelessWidget {
@@ -10,6 +10,7 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: LoginPage(),
     );
   }
@@ -20,12 +21,12 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.all(20.0),
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: 90.0),
-            Text(
+          children: [
+            const SizedBox(height: 90.0),
+            const Text(
               "Start or Join a Meeting",
               style: TextStyle(
                 fontSize: 26,
@@ -33,9 +34,9 @@ class LoginPage extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
             Image.asset("images/zoom1.png"),
-            SizedBox(height: 60.0),
+            const SizedBox(height: 60.0),
             InkWell(
               onTap: () {},
               child: Padding(
@@ -43,51 +44,56 @@ class LoginPage extends StatelessWidget {
                 child: Container(
                   width: double.infinity,
                   height: 50,
-                  decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.circular(20)),
+                  decoration: BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(20)),
                   child: Center(
-                    child: Text(
-                      "Join a Meeting",
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: ((context) {
+                          return p6_join_meeting();
+                        })));
+                      },
+                      child: const Text(
+                        "Join a Meeting",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
             ),
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 InkWell(
                   onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (BuildContext context) {
+                    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
                       return p2_sign_up();
                     }));
                   },
-                  child: Text(
+                  child: const Text(
                     "Sign Up",
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.blue),
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.blue),
                   ),
                 ),
-                Text(
-                  "Sign In",
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.blue),
+                InkWell(
+                  onTap: (() {
+                    Navigator.push(context, MaterialPageRoute(builder: ((context) {
+                      return p5_sign_in();
+                    })));
+                  }),
+                  child: const Text(
+                    "Sign In",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.blue),
+                  ),
                 ),
               ],
             ),
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
           ],
         ),
       ),
@@ -99,50 +105,45 @@ class p2_sign_up extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Sign Up')),
+      appBar: AppBar(title: const Text('Sign Up')),
       body: Container(
-        padding: EdgeInsets.all(4),
+        padding: const EdgeInsets.all(4),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'Sign up',
               style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
             ),
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
             TextField(
               decoration: InputDecoration(
                   labelText: "Email",
-                  prefixIcon: Icon(Icons.email),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0))),
+                  prefixIcon: const Icon(Icons.email),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))),
             ),
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
             TextField(
               decoration: InputDecoration(
                   labelText: "Password",
-                  prefixIcon: Icon(Icons.lock),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0))),
+                  prefixIcon: const Icon(Icons.lock),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))),
               obscureText: true,
             ),
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
             TextField(
               decoration: InputDecoration(
                   labelText: "Confirm Password",
-                  prefixIcon: Icon(Icons.lock),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20.0))),
+                  prefixIcon: const Icon(Icons.lock),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))),
               obscureText: true,
             ),
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
             Container(
               width: double.infinity,
               height: 50,
-              decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.circular(20.0)),
-              child: Center(
+              decoration: BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(20.0)),
+              child: const Center(
                 child: Text(
                   "Sign Up",
                   style: TextStyle(
@@ -152,14 +153,13 @@ class p2_sign_up extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
             Container(
               width: double.infinity,
               height: 50,
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(20.0)),
-              child: Center(
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.grey), borderRadius: BorderRadius.circular(20.0)),
+              child: const Center(
                 child: Text(
                   "Already have an account?",
                   style: TextStyle(
@@ -168,34 +168,31 @@ class p2_sign_up extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
             Row(
               children: [
-                Text('By sign up, i agree to The'),
+                const Text('By sign up, i agree to The'),
                 InkWell(
                   onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (BuildContext context) {
+                    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
                       return p3_Privacy();
                     }));
                   },
-                  child: Text(
+                  child: const Text(
                     'privacy policy',
                     style: TextStyle(color: Colors.blue),
                   ),
                 ),
-                Text('and'),
+                const Text('and'),
                 InkWell(
                   onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (BuildContext context) {
+                    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
                       return p4_TermsPage();
                     }));
                   },
-                  child: Text('Tearms of services',
-                      style: TextStyle(color: Colors.blue)),
+                  child: const Text('Tearms of services', style: TextStyle(color: Colors.blue)),
                 ),
               ],
             )
@@ -211,10 +208,10 @@ class p3_Privacy extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Privacy Policy"),
+          title: const Text("Privacy Policy"),
         ),
         body: ListView(
-          children: <Widget>[
+          children: const <Widget>[
             Padding(
               padding: EdgeInsets.all(16.0),
               child: Text(
@@ -316,10 +313,10 @@ class p4_TermsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Terms of Service"),
+          title: const Text("Terms of Service"),
         ),
         body: ListView(
-          children: <Widget>[
+          children: const <Widget>[
             Padding(
               padding: EdgeInsets.all(16.0),
               child: Text(
@@ -370,6 +367,113 @@ class p4_TermsPage extends StatelessWidget {
               ),
             ),
           ],
+        ));
+  }
+}
+
+class p5_sign_in extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sign Up')),
+      body: Container(
+        padding: const EdgeInsets.all(4),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Sign up',
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(height: 20.0),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: "Email",
+                  prefixIcon: const Icon(Icons.email),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))),
+            ),
+            const SizedBox(height: 20.0),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: "Password",
+                  prefixIcon: const Icon(Icons.lock),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0))),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20.0),
+            const SizedBox(height: 20.0),
+            Container(
+              width: double.infinity,
+              height: 50,
+              decoration: BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(20.0)),
+              child: const Center(
+                child: Text(
+                  "Sign In",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class p6_join_meeting extends StatefulWidget {
+  @override
+  _JoinMeetingPageState createState() => _JoinMeetingPageState();
+}
+
+class _JoinMeetingPageState extends State<p6_join_meeting> {
+  final TextEditingController _meetingIdController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Join Zoom Meeting"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: _meetingIdController,
+                decoration: const InputDecoration(
+                  labelText: "Meeting ID",
+                ),
+              ),
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                  labelText: "Password (optional)",
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              Container(
+                width: double.infinity,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: const Center(
+                    child: Text(
+                  "Join Meeting",
+                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                )),
+              ),
+            ],
+          ),
         ));
   }
 }
